28cd04b772d1bc467e64436dcd7f3ccb0b9e9f60
Kazuya Takami <foundationmeister@outlook.com>
2016-01-15 18:46:07 +0900

    first commit.

---
+++ basic_charts/bar_chart/main.ts
@@ -0,0 +1,22 @@
+class settingChart implements chartMargin {
+
+  constructor(margin: chartMargin) {
+    console.log( margin );
+  }
+}
+
+interface chartMargin {
+  top: number;
+  right: number;
+  left: number;
+  bottom: number;
+}
+
+var chartMargin = {
+  top: 10,
+  right: 10,
+  left: 10,
+  bottom: 10
+};
+
+var chart = new settingChart( chartMargin );
